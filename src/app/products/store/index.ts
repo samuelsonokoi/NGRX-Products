4014d7896c3b5a5b31e61ee54b6b987a19f45e39
Samuelson Okoi <samuelsonokoi@gmail.com>
2020-08-31 14:08:42 +0100

    added dev-tools and products store and reducers

--- src/app/products/store/index.ts
+++ src/app/products/store/index.ts
@@ -4,14 +4,39 @@ import {
   createFeatureSelector,
   createSelector,
   MetaReducer,
+  createReducer,
+  on,
 } from '@ngrx/store';
 import { environment } from '../../../environments/environment';
+import { Product } from '../models/product';
+import { loadProductsSuccess, loadProductsFailure } from './product.actions';
 
 export const productStateFeatureKey = 'productState';
 
-export interface ProductState {}
+export interface ProductState {
+  products: Product[];
+  error: any;
+}
 
-export const reducers: ActionReducerMap<ProductState> = {};
+export const intialState: ProductState = {
+  products: undefined,
+  error: undefined,
+};
+
+export const reducers = createReducer(
+  intialState,
+  on(loadProductsSuccess, (state: ProductState, action) => {
+    return {
+      products: action.products,
+    };
+  }),
+  on(loadProductsFailure, (state: ProductState, action) => {
+    return {
+      products: state.products,
+      error: action.error,
+    };
+  })
+);
 
 export const metaReducers: MetaReducer<ProductState>[] = !environment.production
   ? []
